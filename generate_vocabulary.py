149f153fc2f7453c4d4d93f14cb96f10e39ad40f
Yara Awad <awadyn@bu.edu>
2017-04-15 00:54:38 +0300

    python scripts for processing and word mover's distance

---
+++ generate_vocabulary.py
@@ -0,0 +1,34 @@
+import numpy as np
+import sys
+import os
+
+parsed_malwares_dir = "parsed_malwares"
+
+total_words = []
+
+for root, dirs, filenames in os.walk(parsed_malwares_dir):
+    for filename in filenames:
+            print('adding malware_words of ' + filename + 'to vocabulary...')
+            file = open(os.path.join(root, filename), 'r')
+            data = file.read().splitlines()
+            for word in data:
+                    total_words.append(word)
+
+#
+#for filename in sys.argv[1]:
+#        file = open(filename, "r")
+#        data = file.read().splitlines()
+#        for word in data:
+#                total_words.append(word)
+
+vocabulary = list(set(total_words))
+
+
+print("len(vocabulary): " + str(len(vocabulary)))
+print("vocabulary:")
+print(vocabulary)
+
+output_file = open("vocabulary", "w")
+for word in vocabulary:
+        output_file.write(word)
+        output_file.write("\n")
