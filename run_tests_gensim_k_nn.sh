a0272d520f1a5bf3a43445be186deea83ffc47ee
Yara Awad <awadyn@bu.edu>
2017-05-03 22:01:35 +0300

    correct previous commit with bad script

--- run_tests_gensim_k_nn.sh
+++ run_tests_gensim_k_nn.sh
@@ -1,30 +1,8 @@
 #!/bin/bash
 
-if test "$(ls -A 'parsed_test_malwares')"
-then
-        echo "Moving test files back to parsed_train_malwares"
-        mv "parsed_test_malwares/"* "parsed_train_malwares/"
-fi
-
-
 ./preprocess_for_tests_k_nn.sh
 
-for file in `ls parsed_train_malwares`
-do
-        # choose test malware
-        echo "Choosing test malware ${file}"
-        mv parsed_train_malwares/"${file}" parsed_test_malwares/
-        echo
-        echo
-        # running classification
-        echo "Running classification"
-        python test_gensim.py "parsed_test_malwares/${file}"
-        echo
-        echo
-        # reversing preprocess for next test
-        echo "Reversing preprocessing to prepare for next test"
-        ./reverse_preprocess.sh "parsed_test_malwares/${file}"
-        echo
-        echo
-done
+# running classification
+echo "Running classification"
+python test_gensim_k_nn.py
 
